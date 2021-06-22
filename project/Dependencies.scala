a5c6f2f174202bf9aec63154d8d45eec93ea2074
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-06-22 10:59:27 +0300

    Unify build, workflows (#3)
    * Unify build, workflows
    * set missing names for CI steps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,9 +8,15 @@ object Dependencies {
     val trace4cats = "0.12.0-RC1+156-7ea07b63"
 
     val natchez = "0.1.5"
+
+    val kindProjector = "0.13.0"
+    val betterMonadicFor = "0.3.1"
   }
 
   lazy val trace4catsInject = "io.janstenpickle" %% "trace4cats-inject" % Versions.trace4cats
 
   lazy val natchez = "org.tpolecat" %% "natchez-core" % Versions.natchez
+
+  lazy val kindProjector = ("org.typelevel" % "kind-projector"     % Versions.kindProjector).cross(CrossVersion.full)
+  lazy val betterMonadicFor = "com.olegpy" %% "better-monadic-for" % Versions.betterMonadicFor
 }
