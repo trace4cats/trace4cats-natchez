17a6f316156b43602baa6bed6061f4cef13f8daa
Henry Genet <henry.genet@swisscom.com>
2023-02-03 15:23:18 +0100

    Natchez 0.3

--- modules/natchez/src/main/scala/trace4cats/natchez/Trace4CatsTracer.scala
+++ modules/natchez/src/main/scala/trace4cats/natchez/Trace4CatsTracer.scala
@@ -4,7 +4,7 @@ import cats.effect.kernel.{Resource, Sync}
 import natchez.{EntryPoint, Kernel, Span}
 import trace4cats.ToHeaders
 import trace4cats.kernel.{SpanCompleter, SpanSampler}
-import trace4cats.model.SpanKind
+import trace4cats.natchez.SpanKindConverter.convert
 
 object Trace4CatsTracer {
   def entryPoint[F[_]: Sync](
@@ -13,19 +13,20 @@ object Trace4CatsTracer {
     toHeaders: ToHeaders = ToHeaders.standard
   ): EntryPoint[F] =
     new EntryPoint[F] {
-      override def root(name: String): Resource[F, Span[F]] =
-        Trace4CatsSpan(trace4cats.Span.root(name, SpanKind.Internal, sampler, completer), toHeaders)
+      override def root(name: String, options: Span.Options): Resource[F, Span[F]] =
+        Trace4CatsSpan(trace4cats.Span.root(name, convert(options.spanKind), sampler, completer), toHeaders)
 
-      override def continue(name: String, kernel: Kernel): Resource[F, Span[F]] =
+      override def continue(name: String, kernel: Kernel, options: Span.Options): Resource[F, Span[F]] =
         Trace4CatsSpan(
           toHeaders.toContext(KernelConverter.from(kernel)) match {
-            case None => trace4cats.Span.root(name, SpanKind.Server, sampler, completer)
+            case None => trace4cats.Span.root(name, convert(options.spanKind), sampler, completer)
             case Some(parent) =>
-              trace4cats.Span.child(name, parent, SpanKind.Server, sampler, completer)
+              trace4cats.Span.child(name, parent, convert(options.spanKind), sampler, completer)
           },
           toHeaders
         )
 
-      override def continueOrElseRoot(name: String, kernel: Kernel): Resource[F, Span[F]] = continue(name, kernel)
+      override def continueOrElseRoot(name: String, kernel: Kernel, options: Span.Options): Resource[F, Span[F]] =
+        continue(name, kernel, options)
     }
 }
